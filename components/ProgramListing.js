f8f6cfa2575807708524cf1b6c9d43783c5a9073
Bjørn Vidar Dahle <bvidar@dahle.me>
2022-09-09 20:51:57 +0200

    more info link in program

--- components/ProgramListing.js
+++ components/ProgramListing.js
@@ -85,6 +85,17 @@ const ProgramListing = () => {
 						</p>
 					</div>
 				</div>
+				<div className="flex flex-col lg:w-[430px] pt-12">
+					<h2 className="text-[#666] font-bold text-2xl flex-1">
+						For mere info se her:
+					</h2>
+					<a
+						href="https://www.facebook.com/SteinkjerPride"
+						className="text-blue-400 underline"
+					>
+						Steinkjer Pride Facebook
+					</a>
+				</div>
 			</div>
 		</BlueBox>
 	);
