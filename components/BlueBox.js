10090c9c865ac540c6ed7db8a9cdf95136436580
Bjørn Vidar Dahle <bvidar@dahle.me>
2023-09-08 21:55:31 +0200

    slightly nicer event boxes, full width content boxes, shorter intro and added height

--- components/BlueBox.js
+++ components/BlueBox.js
@@ -1,7 +1,7 @@
 export default function BlueBox({ children }) {
 	return (
-		<section className="ml:0 lg:mr-9 mb-12 lg:rounded-r-[35px] bg-[#BFEDEF] flex justify-center">
-			<div className="p-5 md:p-6 lg:p-12 lg:pr-[80px] lg:pl-[116px] max-w-screen-2xl w-full">
+		<section className="ml:0 bg-[#BFEDEF] flex justify-center">
+			<div className="p-5 md:p-6 lg:p-12 lg:pr-[5rem] lg:pl-[5rem] max-w-screen-2xl w-full">
 				{children}
 			</div>
 		</section>
--- components/PinkBox.js
+++ components/PinkBox.js
@@ -1,6 +1,6 @@
 export default function PinkBox({ children }) {
 	return (
-		<section className="ml-0 lg:ml-9 mb-12 lg:rounded-l-[35px] bg-[#ECC3E7] flex justify-center">
+		<section className="ml-0 bg-[#ECC3E7] flex justify-center">
 			<div className="p-5 md:p6 lg:p-12 lg:pl-[80px] lg:pr-[116px] max-w-screen-2xl w-full">
 				{children}
 			</div>
